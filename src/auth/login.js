4e5a85d264641afef6c4bd496ff5827867383f17
kirrcho <kirrcho6@gmail.com>
2023-06-18 16:03:32 +0300

    updated functionality and startup

--- src/auth/login.js
+++ src/auth/login.js
@@ -4,20 +4,23 @@ import { GoogleLogin } from "@react-oauth/google";
 import { Link, useNavigate } from "react-router-dom";
 import Spinner from "../common/spinner";
 import axios from "axios";
+import { useState } from "react";
 
 function Login(props) {
   const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
 
-  return !props.isGoogleAuthLoading ? (
+  return !props.isGoogleAuthLoading && !isLoading ? (
     <div id="loginModal">
       <h2>Login</h2>
-      <GoogleLogin onSuccess={async (response) => {
+      <GoogleLogin click_listener={() => { setIsLoading(true) }} onSuccess={async (response) => {
         const result = await axios.post(`${process.env.be_url}/auth/login`, { token: response.credential }, {
           'Content-Type': 'application/json'
         });
         if (result.data.isSuccessful) {
           localStorage.setItem('token', result.data.value);
           props.callback();
+          setIsLoading(false);
           navigate("/");
         }
       }}/>
